a6ead414f6b8d67f7ba99ea373e357fcc7f54bb0
JoseFranciscoJunior <jose.franciscoj@outlook.com>
2018-04-05 02:07:36 +0000

    Disabling devise routes (temporarily).

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
 
     def show
         begin
-            @user = User.find(params["id"])
+            @user = User.find(params[:id])
             respond_with @user    
         rescue
             head 404
