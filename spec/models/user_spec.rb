24b9ea951caea093277e87558851655560cb72b5
JoseFranciscoJunior <jose.franciscoj@outlook.com>
2018-04-05 02:13:15 +0000

    Changing double quotes to simple quotes.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  # pending 'add some examples to (or delete) #{__FILE__}'
 
   let(:user) { build(:user) }
 
   # Utilizando shoulda-matchers
-  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_confirmation_of(:password) }
-  it { is_expected.to allow_value("jose.franciscoj@outlook.com").for(:email) }
+  it { is_expected.to allow_value('jose.franciscoj@outlook.com').for(:email) }
   
   # context 'when name is blank' do
-  #   # before (:each) { user.name = " " }
-  #   # before (:all) { user.name = " " }
-  #   before { user.name = " " }
+  #   # before (:each) { user.name = ' ' }
+  #   # before (:all) { user.name = ' ' }
+  #   before { user.name = ' ' }
 
   #   it { expect(user).not_to be_valid }
   # end
